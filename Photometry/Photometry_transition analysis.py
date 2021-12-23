0e31269cd40484d4057afbf712cce28561b5047c
rampagingram <38052204+rampagingram@users.noreply.github.com>
2021-12-23 13:31:19 -0600

    Update Photometry_transition analysis.py

--- Photometry/Photometry_transition analysis.py
+++ Photometry/Photometry_transition analysis.py
@@ -20,7 +20,6 @@ all_name = 'combinedFINAL' ## This is attached to all transition files from a pr
 NREM2REM_name = all_name +'_NREM2REM.txt' ## filename for NREM-to-REM transitions
 NREM2wake_name = all_name+'_NREM2wake.txt' ## filename for NREM-to-wake transitions
 REM2wake_name = all_name +'_REM2wake.txt' ## filename for REM-to-wake transitions
-REM2NREM_name = all_name+'_REM2NREM.txt' ## filename for REM-to-NREM transitions
 wake2NREM_name = all_name+'_wake2NREM.txt' ## filename for wake-to-NREM transitions
 
 zScore = True ##whether to z-score the date or plot raw values. If true, z-score the data
@@ -115,24 +114,19 @@ with open(NREM2REM_name, newline='') as inputfile:
 with open(NREM2wake_name, newline='') as inputfile:
     NREM2wake = list(csv.reader(inputfile))
 with open(REM2wake_name, newline='') as inputfile:
-    REM2wake = list(csv.reader(inputfile))
-with open(REM2NREM_name, newline='') as inputfile:
-    REM2NREM = list(csv.reader(inputfile))    
+    REM2wake = list(csv.reader(inputfile))   
 with open(wake2NREM_name, newline='') as inputfile:
     wake2NREM = list(csv.reader(inputfile))    
 NREM2REM = clean_up_imports(NREM2REM)
 NREM2wake = clean_up_imports(NREM2wake)
 REM2wake = clean_up_imports(REM2wake)
-REM2NREM = clean_up_imports(REM2NREM)
 wake2NREM = clean_up_imports(wake2NREM)
 NREM2REM_avg, NREM2REM_std = avg_animals(NREM2REM)
 NREM2wake_avg, NREM2wake_std = avg_animals(NREM2wake)
 REM2wake_avg, REM2wake_std = avg_animals(REM2wake)
-REM2NREM_avg, REM2NREM_std = avg_animals(REM2NREM)
 wake2NREM_avg, wake2NREM_std = avg_animals(wake2NREM)
 sec = create_sec(NREM2REM_avg)
 plot_transitions(NREM2wake_avg,NREM2wake_std, sec, '_NREM to wake', 'NREM', 'Wake')
 plot_transitions(REM2wake_avg,REM2wake_std, sec, 'REM to wake', 'REM', 'Wake')
-plot_transitions(REM2NREM_avg,REM2NREM_std, sec, 'REM to NREM', 'REM', 'NREM')
 plot_transitions(wake2NREM_avg,wake2NREM_std, sec, 'Wake to NREM', 'Wake', 'NREM')
 plot_transitions(NREM2REM_avg,NREM2REM_std, sec, '_NREM to REM', 'NREM', 'REM')
